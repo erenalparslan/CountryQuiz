e9650da4e23d4f34428fce916afe88ad3c6a02db
erenalparslan <erenalparslan52@gmail.com>
2023-03-20 15:42:45 +0300

    result activity added

--- app/src/main/java/com/erenalparslan/countryquiz/MainActivity.kt
+++ app/src/main/java/com/erenalparslan/countryquiz/MainActivity.kt
@@ -12,15 +12,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val buttonStart: Button = findViewById(R.id.btn_start)
-        val etName: AppCompatEditText = findViewById(R.id.et_name)
-
+        val buttonStart:Button = findViewById(R.id.btn_start)
+        val etName:AppCompatEditText = findViewById(R.id.et_name)
         buttonStart.setOnClickListener {
             if (etName.text.toString().isEmpty()){
                 Toast.makeText(this,"Please Enter Your Name",Toast.LENGTH_SHORT).show()
             }else{
-                val intent = Intent(this,QuizQuestionsActivity::class.java)
+                val intent = Intent(this@MainActivity, QuizQuestionsActivity::class.java)
+                // TODO Pass the name through intent using the constant variable which we have created.
+                intent.putExtra(Constants.USER_NAME, etName.text.toString())
                 startActivity(intent)
+                finish()
             }
         }
     }
